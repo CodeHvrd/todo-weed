59eeb45aba693c942d1a630176cebd217e80afe2
Juanma <juanmaarosales@gmail.com>
2021-10-24 02:18:41 -0300

    Button event created!

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+const event = () => {
+    console.log('Me clickeaste!')
+}
 
 export const Button = ({ content, color }) => {
-    return <button className="button" style={{ backgroundColor: color }} >{content}</button>
+    return <button onClick = {event} className="button" style={{ backgroundColor: color }}>
+        {content}
+        </button>
 }
 
